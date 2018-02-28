d5f8ba530bbeb28856b1ed1e7b8963474021ea19
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-28 17:26:39 +0530

    Create simpcalculation.cpp

---
+++ simpcalculation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int A,B,C,Z;
+   cout<<"A=";
+   cin>>A;
+   cout<<"B=";
+   cin>>B;
+   cout<<"C=";
+   cin>>C;
+   Z=(A*B)/C;
+   cout<<Z;
+   return 0;
+}
